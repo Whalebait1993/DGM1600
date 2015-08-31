db444229b6153709db5e590d863f0f506e2711ae
Jonah Atherley <imwhalebait@gmail.com>
2015-08-31 15:52:26 -0600

    Changes to Unity.  Will pick this up again on Wednesday (September 2nd 2015)

---
+++ New Unity Project/Assets/VariablesAndFunctions.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class VariablesAndFunctions : MonoBehaviour 
+{
+
+
+	int theInt = 5;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		theInt = MultiplyByTwo (theInt);
+		Debug.Log (theInt);
+
+	}
+
+
+	int MultiplyByTwo (int number)
+	{
+		int ret;
+		ret = number * 2;
+		return ret;
+	}
+
+
+
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
