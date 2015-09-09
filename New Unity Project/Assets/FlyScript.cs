0b2062ad5c8774394660ef6443f2c906fca694e1
Jonah Atherley <imwhalebait@gmail.com>
2015-09-09 15:18:32 -0600

    Update Wednesday Sept 9 2015

---
+++ New Unity Project/Assets/FlyScript.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlyScript : MonoBehaviour 
+{
+	private float heightTest = 3f;
+	void Start ()
+	{
+		Debug (transform.position.y);
+
+		if(transform.position.y = heightTest)
+		{
+			Debug.log("I'm about to hit the ground!");
+			FlyAway();
+		}
+
+		else 
+
+		if(transform.position.y >= heightTest)
+		{
+			Debug.Log("I'm flying!");
+		}
+	}
+
+	private void FlyAway()
+	{
+		Debug.log ("Fly Away!");
+		Vector3 newPosition = new Vector3(0,7,0);
+		transform.position.y = newPosition;
+	}
+}
+
+
+
+
