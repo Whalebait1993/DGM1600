6e102c656bef0ea90b91bd188cb4537d3f4c993d
Jonah Atherley <imwhalebait@gmail.com>
2015-09-09 15:29:08 -0600

    again today.

--- New Unity Project/Assets/FlyScript.cs
+++ New Unity Project/Assets/FlyScript.cs
@@ -6,16 +6,16 @@ public class FlyScript : MonoBehaviour
 	private float heightTest = 3f;
 	void Start ()
 	{
-		Debug (transform.position.y);
+		Debug.Log(transform.position.y);
 
-		if(transform.position.y = heightTest)
+		if(transform.position.y <= heightTest)
 		{
-			Debug.log("I'm about to hit the ground!");
+			Debug.Log("I'm about to hit the ground!");
 			FlyAway();
 		}
 
 		else 
-
+		
 		if(transform.position.y >= heightTest)
 		{
 			Debug.Log("I'm flying!");
@@ -24,12 +24,11 @@ public class FlyScript : MonoBehaviour
 
 	private void FlyAway()
 	{
-		Debug.log ("Fly Away!");
+		Debug.Log("Fly Away!");
 		Vector3 newPosition = new Vector3(0,7,0);
-		transform.position.y = newPosition;
+		transform.position = newPosition;//  Transform.position is asking for all 3 coordinate.  Transform.position.y would be asking for only one.
 	}
 }
-
 
 
 
