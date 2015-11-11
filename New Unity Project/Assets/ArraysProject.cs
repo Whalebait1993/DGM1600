6b6bf6e375be77198826cf66a5792e609cf69926
Jonah Atherley <imwhalebait@gmail.com>
2015-11-11 15:32:24 -0700

    Passing off Enums and a couple of things behind it.

---
+++ New Unity Project/Assets/ArraysProject.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ArraysProject : MonoBehaviour 
+
+
+
+{
+
+
+	public string[] Candies = new string[4];
+
+
+
+	void Start () 
+	{
+		Candies[0] = "Chocolate Bar";
+		Candies[1] = "Vanilla Ice Cream";
+		Candies[2] = "Ritz Cracker";
+		Candies[3] = "Bread";
+
+
+		for(int i=0;i<Candies.Length;i++)
+		{
+			Debug.Log ("I Love to eat " + Candies[i] +"'s");
+
+		}
+
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ New Unity Project/Assets/Enums.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enums : MonoBehaviour {
+
+
+	 enum Cellphone {Screen, Case , Power , Battery , Ram};
+
+
+
+	void Start () 
+	{
+		Cellphone iPhone;
+
+		iPhone = Cellphone.Screen;
+
+		iPhone = phonetest(iPhone);
+
+		Debug.Log (iPhone);
+
+	}
+	
+	Cellphone phonetest (Cellphone piece)
+	{
+
+		if (piece == Cellphone.Screen)
+			piece = Cellphone.Case;
+		else if (piece == Cellphone.Power)
+			piece = Cellphone.Ram;
+	
+
+			return piece;
+	}
+
+
+
+
+
+
+	void Update () 
+	{
+	
+	}
+}
