fe113fc2a0b706fd85b07be06a0e8c543a81203c
Jonah Atherley <imwhalebait@gmail.com>
2015-12-07 16:20:27 -0700

    December 7th, 2015.  A lot more refined and better background.  Also started the GUI, but only started.  Currently working on animations.

---
+++ New Unity Project/Assets/Duck Hunt/Duck Hunt Scripts/Bullets.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Bullets : MonoBehaviour {
+
+	public GameObject[] bullets;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		GameManager2.OnSpawnDucks = ResetBullets;
+
+
+
+	}
+
+
+	public void DisableBullet(int index)
+	{
+
+		Bullets [index].SetActive (false);
+
+	}
+
+
+
+
+
+
+
+
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
